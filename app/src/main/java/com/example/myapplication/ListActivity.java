afc3722f2620cae20755d0303bf8a75da2714dfd
Mary Ann Inoc <maryanninoc2190@gmail.com>
2023-03-05 13:03:24 +0800

    03/04/23 INPUTS

--- app/src/main/java/com/example/myapplication/ListActivity.java
+++ app/src/main/java/com/example/myapplication/ListActivity.java
@@ -3,12 +3,30 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
 
 public class ListActivity extends AppCompatActivity {
+    ListView listView;
+    TextView textView;
+
+    String[] listItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        listView = (ListView) findViewById(R.id.listView);
+        listItem = getResources().getStringArray(R.array.array_technology);
+
+
     }
+
 }
