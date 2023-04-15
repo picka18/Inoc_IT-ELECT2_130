a500bd995919daeb9b4df6e00b63fe06fe221514
picka18 <maryanninoc2190@gmail.com>
2023-04-15 18:02:13 +0800

    4-15-2023 | Buttom Navigation

---
+++ bottomnav/src/main/java/com/example/bottomnav/SecondActivity.java
@@ -0,0 +1,33 @@
+package com.example.bottomnav;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.bottomnav.databinding.ActivitySecondBinding;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private ActivitySecondBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        TextView textView = (TextView) findViewById(R.id.text);
+
+        Intent intent = getIntent();
+        String fromMain = intent.getStringExtra("PASS_ME");
+
+        textView.setText(fromMain);
+    }
+}
